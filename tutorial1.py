b80d2c69c5c52d57252d3bb32abc1842750cf55c
zhandavidz <46409638+zhandavidz@users.noreply.github.com>
2020-10-27 10:34:41 -0700

    tutorials 1 and 2

---
+++ tutorial1.py
@@ -0,0 +1,60 @@
+import pygame
+pygame.init()
+
+screen_width = 500
+screen_height = 500
+
+win = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("First Game")
+
+x = 50
+y = 440
+width = 40
+height = 60
+vel = 5
+
+isJump = False
+jumpCount = 10
+
+run = True
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < screen_width - width:
+        x += vel
+
+    if not isJump:
+        if keys[pygame.K_UP] and y > 0:
+            y -= vel
+
+        if keys[pygame.K_DOWN] and y < screen_height - height:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * .5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+    win.fill((0,0,0))
+
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
